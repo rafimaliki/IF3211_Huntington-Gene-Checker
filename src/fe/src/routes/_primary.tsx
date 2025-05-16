cd01d92d3bac382bd3e9738e370dc16797b6b153
Ahmad Rafi Maliki <rafimaliki28@gmail.com>
2025-05-16 09:45:59 +0700

    fix: build errors

--- src/fe/src/routes/_primary.tsx
+++ src/fe/src/routes/_primary.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute, Navigate, Outlet } from '@tanstack/react-router'
+import { createFileRoute, Outlet } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_primary')({
   component: RouteComponent,
